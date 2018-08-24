35851e1010c963d17cd27dca7eee25e73505e9a7
jackcauliflower <42452695+jackcauliflower@users.noreply.github.com>
2018-08-24 18:30:21 -0500

    Initial commit.

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# home page
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+
+# settings page
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
 
 
 if __name__ == '__main__':
